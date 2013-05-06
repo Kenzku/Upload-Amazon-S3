8ed10f1d920a39c588a965fa8166d4405e250d2c
Watke <wsd.2012.ken@gmail.com>
2013-05-06 23:37:25 +0300

    upload to S3 from a web interface succeed

--- routes/s3.js
+++ routes/s3.js
@@ -3,15 +3,19 @@
  * Date: 05/05/2013
  * Time: 13:21
  */
-var nconf = require('nconf');
+var nconf = require('nconf')
+    , Policy = require('../lib/Policy')
+    ;
 
 exports.show = function(req, res){
-        // change to your AWS S3 bucket
-        var ACTION = nconf.get('S3_BUCKET');
-        // I put the key in my local environment variable in .bash_profile
-        var YOUR_AWS_ACCESS_KEY = nconf.get('AWS_ACCESS_KEY');
-        var YOUR_POLICY_DOCUMENT_BASE64_ENCODED = '';
-        var YOUR_CALCULATED_SIGNATURE = '';
+    var aPolicy = new Policy();
+
+    // change to your AWS S3 bucket
+    var ACTION = nconf.get('S3_BUCKET');
+    // I put the key in my local environment variable in .bash_profile
+    var YOUR_AWS_ACCESS_KEY = nconf.get('AWS_ACCESS_KEY');
+    var YOUR_POLICY_DOCUMENT_BASE64_ENCODED = aPolicy.toBase64() || null;
+    var YOUR_CALCULATED_SIGNATURE = aPolicy.getSignature() || null;
 
     res.render('s3',{
         title:'S3 POST Form',
